5a90c5947059db2b42e930125c4e4c52546d6235
Vitaliy Komarov <vitaliy_komarov@epam.com>
2015-07-15 17:17:38 +0400

    init

---
+++ app/scripts/map-init.js
@@ -0,0 +1,58 @@
+var map,
+  myPlacemark;
+
+function init() {
+  map = new ymaps.Map("map", {
+    center: [55.76, 37.64],
+    zoom: 7,
+    controls: [
+      'fullscreenControl'
+      //, 'geolocationControl'
+      , 'typeSelector'
+      //, 'zoomControl'
+    ]
+  });
+
+  myPlacemark = new ymaps.Placemark(
+    [55.76, 37.64],
+    {
+      content: "Москва!",
+      balloonContent: "Столица России"
+    }
+  );
+
+  map.geoObjects.add(myPlacemark);
+
+  //map.controls.add('zoomControl', {float: 'right'});
+
+  var geolocationControl = new ymaps.control.GeolocationControl({
+    options: {
+      //size: 'small',
+      position: {
+        right: 10,
+        top: 50
+      }
+    }
+  });
+
+  var zoomControl = new ymaps.control.ZoomControl({
+    options: {
+      size: 'small',
+      position: {
+        right: 10,
+        top: 90
+      }
+    }
+  });
+
+  map.controls.add(geolocationControl);
+  map.controls.add(zoomControl);
+
+  var myButton = new ymaps.control.Button("Моя кнопка");
+
+  map.controls.add(myButton, {float: 'left'});
+
+}
+
+
+ymaps.ready(init);
---
+++ app/scripts/offices-map-widget.js
@@ -0,0 +1,12 @@
+var w = (function() {
+
+  function init(settings) {
+    console.clear();
+    console.log('Offices widget works!');
+  }
+
+  return {
+    init:init
+  }
+})();
+
