c3f79123872da697dbec72e06d885705a4482870
Vitaly <v-a-komarov@yandex.ru>
2015-07-22 17:23:36 +0400

    readme changed

--- app/scripts/library.js
+++ app/scripts/library.js
@@ -1,19 +1,19 @@
 //
-
-function hasClass(ele,cls) {
-  return !!ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
-}
-
-function addClass(ele,cls) {
-  if (!hasClass(ele,cls)) ele.className += " "+cls;
-}
-
-function removeClass(ele,cls) {
-  if (hasClass(ele,cls)) {
-    var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
-    ele.className=ele.className.replace(reg,' ');
-  }
-}
+//
+//function hasClass(ele,cls) {
+//  return !!ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
+//}
+//
+//function addClass(ele,cls) {
+//  if (!hasClass(ele,cls)) ele.className += " "+cls;
+//}
+//
+//function removeClass(ele,cls) {
+//  if (hasClass(ele,cls)) {
+//    var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
+//    ele.className=ele.className.replace(reg,' ');
+//  }
+//}
 
 function createDOMFromString(string) {
   var tempDiv = document.createElement('div');
@@ -21,15 +21,20 @@ function createDOMFromString(string) {
   return tempDiv.firstChild;
 }
 
-Element.prototype.remove = function() {
-    this.parentElement.removeChild(this);
+if (!Element.prototype.remove) {
+  Element.prototype.remove = function() {
+      this.parentElement.removeChild(this);
+  }
 }
-NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
-    for(var i = this.length - 1; i >= 0; i--) {
-        if(this[i] && this[i].parentElement) {
-            this[i].parentElement.removeChild(this[i]);
-        }
-    }
+
+if (!NodeList.prototype.remove) {
+  NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+      for(var i = this.length - 1; i >= 0; i--) {
+          if(this[i] && this[i].parentElement) {
+              this[i].parentElement.removeChild(this[i]);
+          }
+      }
+  }
 }
 
 function LoadingAnimationBox(el) {
